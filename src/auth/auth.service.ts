437887e54ca2460e101f34e9c26b1f59dcba3f2e
Wojciech Mitrus <wmitrus@gmail.com>
2022-10-23 20:39:53 +0200

    refactor(auth): remove prisma from import as it's global module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
   signup(createAuthDto: CreateAuthDto) {
     return createAuthDto;
   }
