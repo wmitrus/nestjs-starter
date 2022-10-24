d6f8c1f262df21d4a96983e431c42d0ee33c5bf2
Wojciech Mitrus <wmitrus@gmail.com>
2022-10-24 20:53:37 +0200

    chore(prisma): add seed ts file and script in package.json

---
+++ prisma/seed.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.deleteMany();
+  const user1 = await prisma.user.create({
+    data: {
+      email: 'user1@example.com',
+      hash: 'jos8efh498wh4f',
+      firstName: 'First',
+      lastName: 'User',
+    },
+  });
+
+  const user2 = await prisma.user.create({
+    data: {
+      email: 'user2@example.com',
+      hash: 'jos8efh498wh4f',
+      firstName: 'Second',
+      lastName: 'User',
+    },
+  });
+
+  console.log({ user1, user2 });
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
