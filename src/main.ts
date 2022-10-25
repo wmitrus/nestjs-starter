8d2ef501705719e2e04a98e44f483ebf722fd2d0
Wojciech Mitrus <wmitrus@gmail.com>
2022-10-25 10:10:23 +0200

    feat(prisma): implement and add global exception filter for prisma

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common/pipes';
+import { PrismaClientExceptionFilter } from 'src/common/filters/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  -(await app.listen(3100));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // clear the properties are sent by user but not exist in dto
+    }),
+  );
+
+  // Prisma Client Exception Filter for unhandled exceptions
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  await app.listen(3100);
 }
 bootstrap();
