83d1979b9ea7950ed0740b1d9b4160db7c5a9684
Wojciech Mitrus <wmitrus@gmail.com>
2022-10-25 18:44:06 +0200

    chore(app): set ConfigModule as global

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env.prod', '.env'],
+      envFilePath: '.prod.env',
     }),
     AuthModule,
     UserModule,
