c44e83c4a1bd287493c2ba9bb927e09f9fd00d7b
Wojciech Mitrus <wmitrus@gmail.com>
2022-11-02 17:07:54 +0100

    feat(user): add GetMe method resolving user data from token

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtGuard } from 'src/auth/guard';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { RemovePasswordInterceptor } from './interceptor/RemovePassword.interceptor';
 
+@UseInterceptors(RemovePasswordInterceptor)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtGuard)
+  @Get('me')
+  findMe(@GetUser() user: User): User {
+    return user;
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -20,6 +41,11 @@ export class UserController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
+  }
+
+  @Post('search')
+  search(@Body() dto: Partial<User>) {
+    return this.userService.search(dto);
   }
 
   @Patch(':id')
